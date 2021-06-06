c34bb689123d97ddbe02626306d8f99b694f0819
zjyl1994 <me@zjyl1994.com>
2021-06-07 00:21:30 +0800

    add libpaging

---
+++ libpaging/libpaging.go
@@ -0,0 +1,71 @@
+package libpaging
+
+import (
+	"strconv"
+	"strings"
+)
+
+type PagingContext struct {
+	Current int
+	NextURL string
+	PrevURL string
+	PageURL map[int]string
+}
+
+func Default() Paging {
+	return Paging{
+		PagingBtnNum:        5,
+		PageNoPlaceholder:   "$pageNo$",
+		PageSizePlaceholder: "$pageSize$",
+		DefaultPageSize:     10,
+	}
+}
+
+type Paging struct {
+	PagingBtnNum        int
+	PageNoPlaceholder   string
+	PageSizePlaceholder string
+	PagingURL           string
+	DefaultPageSize     int
+}
+
+func (p Paging) SQLHelper(pageNo, pageSize int) (limit, offset int) {
+	if pageNo <= 0 {
+		pageNo = 1
+	}
+	if pageSize <= 0 {
+		pageSize = p.DefaultPageSize
+	}
+	return pageSize, (pageNo - 1) * pageSize
+}
+
+func (p Paging) ButtonHelper(pageNo, pageSize int, totalCount int64) PagingContext {
+	totalPage := (int(totalCount) + pageSize - 1) / pageSize
+	pages := make(map[int]string)
+	var ret PagingContext
+	ret.Current = pageNo
+	if pageNo > 1 {
+		ret.PrevURL = p.buildPagingURL(pageNo-1, pageSize)
+	}
+	if pageNo < totalPage {
+		ret.NextURL = p.buildPagingURL(pageNo+1, pageSize)
+	}
+	if totalPage > p.PagingBtnNum {
+		pagingBtnStartAt := (p.PagingBtnNum+1)/2 - (p.PagingBtnNum / 2)
+		for i := pagingBtnStartAt; i <= p.PagingBtnNum; i++ {
+			pages[i] = p.buildPagingURL(i, pageSize)
+		}
+	} else {
+		for i := 1; i <= totalPage; i++ {
+			pages[i] = p.buildPagingURL(i, pageSize)
+		}
+	}
+
+	ret.PageURL = pages
+	return ret
+}
+
+func (p Paging) buildPagingURL(pageNo, pageSize int) string {
+	tmpStr := strings.ReplaceAll(p.PagingURL, p.PageNoPlaceholder, strconv.Itoa(pageNo))
+	return strings.ReplaceAll(tmpStr, p.PageSizePlaceholder, strconv.Itoa(pageSize))
+}
